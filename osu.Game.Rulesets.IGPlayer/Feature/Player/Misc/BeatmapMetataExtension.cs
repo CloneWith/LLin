ca0b740dc2aff0eafb1ff0e08b802fe8f4e14b2d
MATRIX-feather <midnightcarnival@outlook.com>
2024-09-21 18:45:28 +0800

    misc: 让BeatmapName使用RomanisableString显示歌曲名称 misc: 添加一个针对BeatmapName里名称FillFlow虚空增高的缓解方案

--- osu.Game.Rulesets.IGPlayer/Feature/Player/Misc/BeatmapMetataExtension.cs
+++ osu.Game.Rulesets.IGPlayer/Feature/Player/Misc/BeatmapMetataExtension.cs
@@ -1,12 +1,23 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
 
 namespace osu.Game.Rulesets.IGPlayer.Feature.Player.Misc
 {
     public static class BeatmapMetataExtension
     {
+        public static RomanisableString GetTitleRomanisable(this BeatmapMetadata metadata)
+        {
+            return new RomanisableString(metadata.TitleUnicode, metadata.Title);
+        }
+
+        public static RomanisableString GetArtistRomanisable(this BeatmapMetadata metadata)
+        {
+            return new RomanisableString(metadata.ArtistUnicode, metadata.Artist);
+        }
+
         public static string GetTitle(this BeatmapMetadata metadata)
         {
             return string.IsNullOrEmpty(metadata.TitleUnicode)
