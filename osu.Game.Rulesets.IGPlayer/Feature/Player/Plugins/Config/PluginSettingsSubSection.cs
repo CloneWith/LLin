a2c2327788d6ecae46aee927832ace735aa6fbe8
MATRIX-feather <midnightcarnival@outlook.com>
2024-11-25 20:13:31 +0800

    enhance: 单独创建一个SettingsSubPanel展示Hikariii插件设置

--- osu.Game.Rulesets.IGPlayer/Feature/Player/Plugins/Config/PluginSettingsSubSection.cs
+++ osu.Game.Rulesets.IGPlayer/Feature/Player/Plugins/Config/PluginSettingsSubSection.cs
@@ -1,9 +1,11 @@
 using System;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Overlays.Settings;
 using osu.Game.Rulesets.IGPlayer.Feature.Player.Interfaces.Plugins;
+using osuTK;
 
 namespace osu.Game.Rulesets.IGPlayer.Feature.Player.Plugins.Config
 {
@@ -28,7 +30,7 @@ namespace osu.Game.Rulesets.IGPlayer.Feature.Player.Plugins.Config
         }
     }
 
-    public partial class PluginSettingsSubsection : SettingsSubsection
+    public partial class PluginSettingsSubsection : SettingsSection
     {
         private readonly LLinPlugin plugin;
 
@@ -41,7 +43,16 @@ namespace osu.Game.Rulesets.IGPlayer.Feature.Player.Plugins.Config
             RelativeSizeAxes = Axes.X;
         }
 
-        protected override LocalisableString Header => plugin.Name;
+        public override Drawable CreateIcon()
+        {
+            return new SpriteIcon
+            {
+                Icon = FontAwesome.Solid.Atom,
+                Size = new Vector2(18)
+            };
+        }
+
+        public override LocalisableString Header => plugin.Name;
 
         [BackgroundDependencyLoader]
         private void load(LLinPluginManager pluginManager)
